61f61e75b17445e85a3391faae8ae9838259dcde
STYLESTHEKING <88886849+STYLESTHEKING@users.noreply.github.com>
2022-10-23 23:47:46 -0500

    Arreglos

--- main.ts
+++ main.ts
@@ -10,13 +10,13 @@ function renderTable(series: serie[]): void {
     trElement.innerHTML = `<td>${serie.id}</td>
                            <td style="color: #0051ff;">${serie.name}</td>
                            <td>${serie.channel}</td>
-                           <td>${serie.season}</td>`;
+                           <td>${serie.seasons}</td>`;
     seriestbody.appendChild(trElement);
   });
 }
 function AverageSeasons(series: serie[]): void {
   let totalS: number = 0;
   let avgss: HTMLElement = document.getElementById("average-seasons")!;
-  series.forEach((serie) => {(totalS += serie.season)});
+  series.forEach((serie) => {(totalS += serie.seasons)});
   avgss.innerHTML = `${(totalS / series.length)}`;
 }
--- serie.ts
+++ serie.ts
@@ -3,16 +3,16 @@ export class serie {
     id: number;
     name: string;
     channel: string;
-    season: number;
+    seasons: number;
     description: string;
     review: string;
     picture: string;
   
-    constructor(id: number, name: string, channel: string, season: number, description: string, review: string, picture: string) {
+    constructor(id: number, name: string, channel: string, seasons: number, description: string, review: string, picture: string) {
       this.id = id;
       this.name = name;
       this.channel = channel;
-      this.season = season;
+      this.seasons = seasons;
       this.description = description;
       this.review = review;
       this.picture= picture;
